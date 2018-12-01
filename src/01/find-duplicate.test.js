970626501c8631d63ba5276727d538fc8e05efeb
MattMorgis <matt@rappiddevelopment.com>
2018-12-01 17:58:03 -0500

    test: duplicate not found at end of stream

--- src/01/find-duplicate.test.js
+++ src/01/find-duplicate.test.js
@@ -18,3 +18,9 @@ it("+1 -1 reaches 0 twice", async () => {
   const result = await findDuplicate(stream);
   assert.equal(result, 0);
 });
+
+it("+3, +3, +4, -2, -4 reaches 10 twice", async () => {
+  const stream = mockFileStream("+3\n", "+3\n", "+4\n", "+2\n", "-4\n");
+  const result = await findDuplicate(stream);
+  assert.equal(result, 10);
+});
