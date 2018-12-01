cdea204a9ac9976898d6cfb3aaa61f502f3685f7
MattMorgis <matt@rappiddevelopment.com>
2018-12-01 18:24:40 -0500

    feat: logging how many times it's reading the file

--- src/01/find-duplicate.js
+++ src/01/find-duplicate.js
@@ -4,10 +4,12 @@ const PassThrough = require("stream").PassThrough;
 const findDuplicates = async stream => {
   let frequency = 0;
   const frequenciesFound = [0];
-
+  let iter = 0;
   while (true) {
     let frozenStream = stream.pipe(new PassThrough());
+    iter += 1;
     for await (const number of streamToNumbers(stream)) {
+      process.stdout.write(`reading file ${iter} times\r`);
       frequency += number;
       if (frequenciesFound.includes(frequency)) {
         return frequency;
