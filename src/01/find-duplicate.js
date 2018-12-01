a581cc0ea9681c47ae33d621cb2076f6eb581605
MattMorgis <matt@rappiddevelopment.com>
2018-12-01 18:28:37 -0500

    refactor: clone stream method

--- src/01/find-duplicate.js
+++ src/01/find-duplicate.js
@@ -1,19 +1,30 @@
 const streamToNumbers = require("./stream-to-lines");
 const PassThrough = require("stream").PassThrough;
 
+const clone = stream => {
+  return stream.pipe(new PassThrough());
+};
+
 const findDuplicates = async stream => {
   let frequency = 0;
   const frequenciesFound = [0];
   let iter = 0;
+
   while (true) {
-    let frozenStream = stream.pipe(new PassThrough());
+    // clone stream and put in cold storage
+    // in case we need to re-read inputs.
+    let frozenStream = clone(stream);
     iter += 1;
+
     for await (const number of streamToNumbers(stream)) {
       process.stdout.write(`reading file ${iter} times\r`);
+
       frequency += number;
+
       if (frequenciesFound.includes(frequency)) {
         return frequency;
       }
+
       frequenciesFound.push(frequency);
     }
     stream = frozenStream;
