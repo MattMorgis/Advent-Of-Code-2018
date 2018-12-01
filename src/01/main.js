9db77c2913b7b3f7e372cc686c517e10e27a421a
MattMorgis <matt@rappiddevelopment.com>
2018-12-01 18:13:10 -0500

    feat: call part 2

--- src/01/main.js
+++ src/01/main.js
@@ -1,14 +1,19 @@
 const fs = require("fs");
 
 const sum = require("./sum");
+const findDuplicate = require("./find-duplicate");
+
+const fileStream = () => {
+  return fs.createReadStream(__dirname + "/input.txt", {
+    encoding: "utf-8",
+    highWaterMark: 256
+  });
+};
 
 const main = async () => {
   try {
-    const fileStream = fs.createReadStream(__dirname + "/input.txt", {
-      encoding: "utf-8",
-      highWaterMark: 256
-    });
-    console.log(await sum(fileStream));
+    console.log({sum: await sum(fileStream())});
+    console.log({duplicate: await findDuplicate(fileStream())});
   } catch (e) {
     console.log(e.message);
     process.exit(-1);
