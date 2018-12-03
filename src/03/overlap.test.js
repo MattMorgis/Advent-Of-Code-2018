f2e8a466306112e9c92e3baf9105f3bc6a15d9bd
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2018-12-03 15:59:44 -0500

    test: find unique claim

--- src/03/overlap.test.js
+++ src/03/overlap.test.js
@@ -1,17 +1,15 @@
 const assert = require("assert");
+const Readable = require("stream").Readable;
 
-const overlap = require("./overlap");
+const {overlap, unique} = require("./overlap");
 
 const mockFileStream = (...claims) => {
-  const mockStream = {
-    [Symbol.asyncIterator]: () => {
-      return {
-        next: () => ({
-          done: claims.length === 0,
-          value: claims.shift()
-        })
-      };
+  const mockStream = new Readable();
+  mockStream._read = () => {
+    for (const claim of claims) {
+      mockStream.push(claim);
     }
+    mockStream.push(null);
   };
   return mockStream;
 };
@@ -24,4 +22,14 @@ it("claims overlap should equal 4", async () => {
   );
   const result = await overlap(stream);
   assert.equal(result, 4);
+});
+
+it("unique claim should equal 3", async () => {
+  const stream = mockFileStream(
+    "#1 @ 1,3: 4x4\n",
+    "#2 @ 3,1: 4x4\n",
+    "#3 @ 5,5: 2x2\n"
+  );
+  const result = await unique(stream);
+  assert.equal(result, 3);
 });
