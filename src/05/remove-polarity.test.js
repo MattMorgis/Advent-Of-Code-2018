5741d39a65df32d416c208fa66b86ac62b02c50c
MattMorgis <matt@rappiddevelopment.com>
2018-12-09 12:19:10 -0500

    test: abBA reacts and destroys itselfs

--- src/05/remove-polarity.test.js
+++ src/05/remove-polarity.test.js
@@ -6,3 +6,9 @@ it("aA reacts and returns 0", () => {
   const result = removePolarity(polymer);
   assert.equal(result, 0);
 });
+
+it("abBA reacts and returns 0", () => {
+  const polymer = "abBA";
+  const result = removePolarity(polymer);
+  assert.equal(result, 0);
+});
