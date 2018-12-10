c88654c88fece76f562e6a4f46a9e104f0e206c5
MattMorgis <matt@rappiddevelopment.com>
2018-12-09 21:02:07 -0500

    feat: remove polarity from string

--- src/05/remove-polarity.js
+++ src/05/remove-polarity.js
@@ -1,5 +1,38 @@
+const isUpperCase = letter => letter === letter.toUpperCase();
+const isLowerCase = letter => letter === letter.toLowerCase();
+const lettersAreEqual = (a, b) => a.toUpperCase() === b.toUpperCase();
+
 const removePolarity = polymer => {
-  return 0;
+  polymer = [...polymer];
+
+  let restart = true;
+
+  while (restart) {
+    let found = false;
+
+    for (const [i, letter] of polymer.entries()) {
+      if (i === polymer.length - 1) {
+        break;
+      }
+      const nextLetter = polymer[i + 1];
+
+      if (
+        (isLowerCase(letter) && isUpperCase(nextLetter)) ||
+        (isUpperCase(letter) && isLowerCase(nextLetter))
+      ) {
+        if (lettersAreEqual(letter, nextLetter)) {
+          polymer.splice(i, 2);
+          found = true;
+          break;
+        }
+      }
+    }
+
+    if (!found) {
+      restart = false;
+    }
+  }
+  return polymer.length;
 };
 
 module.exports = removePolarity;
