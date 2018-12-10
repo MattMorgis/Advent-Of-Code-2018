72119473163160307a02e3d2ea52496ef0ad8c7a
MattMorgis <matt@rappiddevelopment.com>
2018-12-09 22:16:04 -0500

    feat: run part 2

--- src/05/main.js
+++ src/05/main.js
@@ -1,14 +1,17 @@
 const fs = require("fs").promises;
-const removePolarity = require("./remove-polarity");
+const {removePolarity, bestPolarity} = require("./remove-polarity");
 
 const main = async () => {
   try {
     const input = await fs.readFile(__dirname + "/input.txt", {
       encoding: "utf-8"
     });
-    const part1 = removePolarity(input);
 
+    const part1 = removePolarity(input);
     console.log({part1});
+
+    const part2 = bestPolarity(input);
+    console.log({part2});
   } catch (e) {
     console.log(e.message);
     process.exit(-1);
