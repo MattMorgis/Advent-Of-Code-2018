a84e406b073440f075ac9903527b4e1266949ce3
MattMorgis <matt@rappiddevelopment.com>
2018-12-09 21:03:51 -0500

    test: more example cases

--- src/05/remove-polarity.test.js
+++ src/05/remove-polarity.test.js
@@ -18,3 +18,21 @@ it("abAB doesn't react and returns 4", () => {
   const result = removePolarity(polymer);
   assert.equal(result, 4);
 });
+
+it("aBbC returns 2", () => {
+  const polymer = "aBbC";
+  const result = removePolarity(polymer);
+  assert.equal(result, 2);
+});
+
+it("aabAAB returns 6", () => {
+  const polymer = "aabAAB";
+  const result = removePolarity(polymer);
+  assert.equal(result, 6);
+});
+
+it("dabAcCaCBAcCcaDA returns 10", () => {
+  const polymer = "dabAcCaCBAcCcaDA";
+  const result = removePolarity(polymer);
+  assert.equal(result, 10);
+});
